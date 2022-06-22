57ba674fa7cd1b7587391d81142033f4815e60b8
rupeshPabba <96829415+rupeshPabba@users.noreply.github.com>
2022-06-22 16:06:00 +0530

    Update loginout.py

--- loginout.py
+++ loginout.py
@@ -6,6 +6,7 @@ def login():
     global t1,name_info,id_info
     name_info=E1.get()
     id_info=E2.get()
+    t1=time.time()
     print(name_info)
     print(id_info)
 
@@ -14,9 +15,11 @@ def login():
 def logout():
     global t2
     t2=time.time()
-    f=time.perf_counter()
-    print(f)
-    root.destroy()
+    f=t2-t1
+    f=f/3600
+    print(f,end=" ")
+    print("hr")
+    
     
 
 
@@ -30,8 +33,8 @@ E1=tk.Entry(root,textvariable=name_var)
 E1.grid(row=1,column=2)
 E2=tk.Entry(root,textvariable=pass_var)
 E2.grid(row=2,column=2)
-b1=tk.Button(root,text="LOGIN",fg="black",command=login)
+b1=tk.Button(root,text="LOGIN",fg="white",bg="blue",command=login)
 b1.grid(row=4,column=1)
-b2=tk.Button(root,text="LOGOUT",fg="black",command=logout)
+b2=tk.Button(root,text="LOGOUT",fg="white",bg="blue",command=logout)
 b2.grid(row=4,column=2)
 root.mainloop()
